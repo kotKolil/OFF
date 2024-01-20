c8c5af9f9145ca6bae9406e12dd2c81fb217b303
Tom Fox <kot3854@yandex.ru>
2024-01-20 15:46:39 +0300

    added  token generation and cheking function in user class

--- classes/tools.py
+++ classes/tools.py
@@ -2,6 +2,15 @@ from random import *
 import datetime
 from .loggers import *
 from .database import *
+import hashlib
+import os
+
+sample= '1h9K8L9h5d5v5Z4q7'
+
+
+def generate_token(s1:str="vasya228", s2:str="12345"):
+    token = hashlib.sha256(str.encode(s1 + s2 + os.urandom(16).hex())).hexdigest()
+    return token
 
 def get_current_time():
     tim = str(datetime.datetime.now())
@@ -37,18 +46,19 @@ def initialise_database(db:object):
         #creating table, which represents the forum class
         #structure of table forum : name;date_of_creasting;id
 
-        #structure of table user password|user|is_admin|is_banned|logo_path|citate|time_of_join
+        #structure of table user password|user_id|is_admin|is_banned|logo_path|citate|time_of_join|token
 
         db.excute_query(""" 
 
                 CREATE TABLE IF NOT EXISTS user (
                     password TEXT,
-                    user TEXT UNIQUE,
+                    user_id text UNIQUE,
                     is_admin BOOLEAN,
                     is_banned BOOLEAN,
                     logo_path TEXT,
                     citate TEXT,
                     time_of_join TEXT
+                    token TEXT UNIQUE NOT NULLS,
                 )
                          
                          
@@ -102,7 +112,69 @@ def initialise_database(db:object):
         
     elif db.__class__ == postgres_db:
 
-        pass
+        # sqlite3 didn't handle date type, so date_of_creation and other similar variables must be like YYYY-MM-DD HH:MM:SS.SSS
+        #creating table, which represents the forum class
+        #structure of table forum : name;date_of_creasting;id
+
+        #structure of table user password|user_id|is_admin|is_banned|logo_path|citate|time_of_join
+
+        db.excute_query(""" 
+
+                CREATE TABLE IF NOT EXISTS user (
+                    password TEXT,
+                    user_id text UNIQUE,
+                    is_admin BOOLEAN,
+                    is_banned BOOLEAN,
+                    logo_path TEXT,
+                    citate TEXT,
+                    time_of_join date
+                )
+                         
+                         
+                         """)
+
+        db.excute_query("""
+
+    CREATE TABLE IF NOT EXISTS forum (
+        name TEXT,
+        date_of_creation date,
+        id TEXT
+    )
+
+                        """)
+        
+        #creating table threads, which represents the thread class
+        #structure of table: id_forum|id_thread|name|time_of_creation|author|decrpt (description)
+
+        db.excute_query("""
+                        
+    CREATE TABLE IF NOT EXISTS thread (
+        id_forum TEXT REFERENCES forum(id),
+        id_thread TEXT UNIQUE PRIMARY KEY,
+        time_of_creation date,
+        author TEXT REFERENCES user(user),
+        decrpt TEXT
+    )
+
+                        
+""")
+
+
+        #creating table messages, which represents the messages class
+        #structure of table messages : |id_thd|mess_id|author|text|time_of_publication
+
+        db.excute_query(""" 
+                        
+    CREATE TABLE IF NOT EXISTS messages (
+        id_thd TEXT REFERENCES thread(id_thread),
+        mess_id TEXT UNIQUE PRIMARY KEY,
+        author TEXT REFERENCES user(user),
+        text_of_publication TEXT,
+        time_of_publication date
+    )
+
+
+                        """)
 
 
 
--- classes/user_class.py
+++ classes/user_class.py
@@ -1,17 +1,22 @@
 from .tools import *
 
 class user:
-    #structure of table user password|user|is_admin|is_bannes|logo_path|citate|time_of_join
-    def __init__(self, password, user, is_admin, is_banned,logo_path,citate,time_of_join, db):
+    #structure of table user password|user|is_admin|is_bannes|logo_path|citate|time_of_join|token
+    def __init__(self, password, user_id, is_admin, is_banned,logo_path,citate,time_of_join, db):
         self.time_of_join = get_current_time()
         self.password = password
-        self.user = user
+        self.user_id = user_id
         self.is_admin = is_admin
         self.is_banned = is_banned
         self.logo_path = logo_path
         self.citate = citate
+        self.token = generate_token(self.user_id, self.password)
         
-
+    def check_token(self, token, user):
+        if self.db_execute(f"SELECT user FROM  user WHERE token = '{token}'") == user:
+            return 1
+        else:
+            return 0
     
 
         
