8839a802e2c054c9dbb79666603a9c949e2736c2
kotkolil <uran54321@yandex.ru>
2024-03-12 23:40:47 +0300

    edits

--- classes/database.py
+++ classes/database.py
@@ -18,13 +18,15 @@ class sql_lite3_db:
         return result
     
 
-class postgres_db(abc_db):
+class postgres_db:
     def __init__(self, port=5432, password="admin", user="admin", name="",host="127.0.0.1"):
         self.port = port
 	self.password = password
 	self.user = user
 	self.name = name
 	self.host = host
+	#initialisng database
+
 
     def execute_query(self, query):
         connection = psql.connect(user=self.user,
